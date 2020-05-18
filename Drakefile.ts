777a2554e80a4641abb98df67bf911a20c8fe8c9
kingdaro <kingdaro@gmail.com>
2020-05-18 18:01:41 -0500

    Drakefile: reduce repetition

--- Drakefile.ts
+++ Drakefile.ts
@@ -1,16 +1,16 @@
 import { run, sh, task } from "https://deno.land/x/drake@v1.0.0/mod.ts"
 
+const cmd = [
+  "deno",
+  "run",
+  "--allow-read",
+  "--allow-net",
+  "--allow-env",
+  "src/main.ts",
+]
+
 task("dev", [], async () => {
-  const runOptions: Deno.RunOptions = {
-    cmd: [
-      "deno",
-      "run",
-      "--allow-read",
-      "--allow-net",
-      "--allow-env",
-      "src/main.ts",
-    ],
-  }
+  const runOptions: Deno.RunOptions = { cmd }
   let process = Deno.run(runOptions)
 
   for await (const change of Deno.watchFs("src", { recursive: true })) {
@@ -20,7 +20,7 @@ task("dev", [], async () => {
 })
 
 task("start", [], async () => {
-  await sh("deno run --allow-read --allow-net --allow-env src/main.ts")
+  await sh(cmd.join(" "))
 })
 
 task("format", [], async () => {
